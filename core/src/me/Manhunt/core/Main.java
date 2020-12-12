3f4194e2a796284b899c4408405901f0d26a78bc
Thijs Boom <twboom@hotmail.nl>
2020-12-12 16:07:10 +0100

    Added a test command

--- core/src/me/Manhunt/core/Main.java
+++ core/src/me/Manhunt/core/Main.java
@@ -1,7 +1,44 @@
 package me.Manhunt.core;
 
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin{
+	
+	@Override
+	public void onEnable() {
+		// Startup
+		// Reloads
+		// Plugin reload
+	}
+	
+	@Override
+	public void onDisable() {
+		// Shutdown
+		// Reloads
+		// Plugin reloads
+	}
+	
+	// /hello <-- Hey Welcome!
+	
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		
+		if (label.equalsIgnoreCase("hello")) {
+			if (sender instanceof Player) {
+				// Sender is player
+				Player player = (Player) sender;
+				player.sendMessage(ChatColor.LIGHT_PURPLE + "Hey player!");
+			}
+			else {
+				// Sender is console
+				sender.sendMessage("Hey!");
+			}
+		}
+		
+		return false;
+	}
 
 }
